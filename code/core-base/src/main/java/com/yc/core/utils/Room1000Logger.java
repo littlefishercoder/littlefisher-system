c63f207b73fc39bf8ac245d03fc3a8ba93aac666
littlefisher <505516920@qq.com>
2017-01-03 15:17:16 +0800

    #5 新增公司自己的log代码

--- code/core-base/src/main/java/com/yc/core/utils/Room1000Logger.java
+++ code/core-base/src/main/java/com/yc/core/utils/Room1000Logger.java
@@ -1,7 +1,9 @@
 package com.yc.core.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 /**
  * Description: 日志管理类 
@@ -14,94 +16,37 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public final class Room1000Logger {
     
-    /**
-     * logger
-     */
+    /** logger */
     protected Logger logger;
-
+    
+    /** loggerCache 缓存 */
+    private static Map<Class<?>, Room1000Logger> loggerCache = new HashMap<Class<?>, Room1000Logger>();
+    
     /**
-     * 将Log类封装成单实例的模式，独立于其他类。以后要用到日志的地方只要获得Log的实例就可以方便使用
-     */
-    private static Room1000Logger log;
-
-    /**
-     * 构造函数，用于初始化Logger配置需要的属性
+     * 私有构造函数
+     * 
      * @param logger logger
      */
     private Room1000Logger(Logger logger) {
-        // 获得当前目录路径
-        // String filePath=this.getClass().getResource("/").getPath();
-        // 找到log4j.properties配置文件所在的目录(已经创建好)
-        // filePath=filePath.substring(1).replace("bin", "src");
-        // 获得日志类logger的实例
-        // loger=Logger.getLogger(this.getClass());
-        String ycHome = StringUtil.isNotEmpty(System.getenv("YC_HOME")) ? System.getenv("YC_HOME") : System.getProperty("YC_HOME");
-        String filePath = ycHome + "/properties/log4j.properties";
         this.logger = logger;
-
-        // logger所需的配置文件路径 D:\\publish2\\log4j.properties
-        PropertyConfigurator.configureAndWatch(filePath);
     }
-
-    /**
-     * 获取构造器，根据类初始化Logger对象
-     * 
-     * @param clazz Class对象
-     * @return Logger对象
-     */
-    public static Room1000Logger getLogger(Class<?> clazz) {
-        if (log != null) {
-            return log;
-        }
-        else {
-            return new Room1000Logger(Logger.getLogger(clazz));
-        }
-    }
-
-//    /**
-//     * 
-//     * Description:  该事件ID包含当前时间和主机IP,是标示日志时间的唯一符
-//     * 
-//     * @author jinyanan
-//     *
-//     * @return eventId
-//     */
-//    protected String eventId() {
-//        // 获取当前时间
-//        // 如果不需要格式,可直接用dt,dt就是当前系统时间
-//        Date dt = new Date();
-//        // 设置显示格式,24小时制
-//        DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-//        String nowTime = df.format(dt); // 用DateFormat的format()方法在dt中获取并以yyyy/MM/dd HH:mm:ss格式显示
-//
-//        String nowIp;
-//        try {
-//            InetAddress addr = InetAddress.getLocalHost();
-//            nowIp = addr.getHostAddress();
-//        }
-//        catch (Exception ex) {
-//            nowIp = "";
-//        }
-//        nowTime = nowTime.replaceAll("/", "");
-//        nowTime = nowTime.replaceAll(" ", "");
-//        nowTime = nowTime.replaceAll(":", "");
-//        nowIp = nowIp.replace(".", "");
-//        String nowId = nowTime + nowIp;
-//        return nowId;
-//    }
-
+    
     /**
      * 
-     * Description:  debug
+     * Description: 获取Room1000Logger实例，并进行缓存
      * 
      * @author jinyanan
      *
-     * @param content content
+     * @param clazz clazz
+     * @return Room1000Logger
      */
-    public void debug(String content) {
-        if (logger.isDebugEnabled()) {
-            logger.debug(content);
+    public static Room1000Logger getLogger(Class<?> clazz) {
+        if (loggerCache.get(clazz) != null) {
+            return loggerCache.get(clazz);
         }
+        Room1000Logger room1000Logger = new Room1000Logger(Logger.getLogger(clazz));
+        loggerCache.put(clazz, room1000Logger);
+        return room1000Logger;
     }
     
     /**
@@ -124,26 +69,15 @@ public final class Room1000Logger {
      * 
      * @author jinyanan
      *
+     * @param msg msg
      * @param t t
      */
-    public void debug(Throwable t) {
+    public void debug(Object msg, Throwable t) {
         if (logger.isDebugEnabled()) {
-            logger.debug(t.getMessage(), t);
+            logger.debug(msg, t);
         }
     }
 
-    /**
-     * 
-     * Description:  fatal
-     * 
-     * @author jinyanan
-     *
-     * @param content content
-     */
-    public void fatal(String content) {
-        logger.fatal(content);
-    }
-    
     /**
      * 
      * Description: fatal
@@ -161,27 +95,14 @@ public final class Room1000Logger {
      * Description: fatal
      * 
      * @author jinyanan
-     *
+     * 
+     * @param msg msg
      * @param t t
      */
-    public void fatal(Throwable t) {
-        logger.fatal(t.getMessage(), t);
+    public void fatal(Object msg, Throwable t) {
+        logger.fatal(msg, t);
     }
 
-    /**
-     * 
-     * Description:  info
-     * 
-     * @author jinyanan
-     *
-     * @param content content
-     */
-    public void info(String content) {
-        if (logger.isInfoEnabled()) {
-            logger.info(content);
-        }
-    }
-    
     /**
      * 
      * Description: info
@@ -202,24 +123,13 @@ public final class Room1000Logger {
      * 
      * @author jinyanan
      *
+     * @param msg msg
      * @param t t
      */
-    public void info(Throwable t) {
+    public void info(Object msg, Throwable t) {
         if (logger.isInfoEnabled()) {
-            logger.info(t.getMessage(), t);
+            logger.info(msg, t);
         }
-    }
-
-    /**
-     * 
-     * Description:  warn
-     * 
-     * @author jinyanan
-     *
-     * @param content content
-     */
-    public void warn(String content) {
-        logger.warn(content);
     }
     
     /**
@@ -240,24 +150,13 @@ public final class Room1000Logger {
      * 
      * @author jinyanan
      *
+     * @param msg msg
      * @param t t
      */
-    public void warn(Throwable t) {
-        logger.warn(t.getMessage(), t);
+    public void warn(Object msg, Throwable t) {
+        logger.warn(msg, t);
     }
 
-    /**
-     * 
-     * Description:  error
-     * 
-     * @author jinyanan
-     *
-     * @param content content
-     */
-    public void error(String content) {
-        logger.error(content);
-    }
-    
     /**
      * 
      * Description: error
@@ -276,10 +175,11 @@ public final class Room1000Logger {
      * 
      * @author jinyanan
      *
+     * @param msg msg
      * @param t t
      */
-    public void error(Throwable t) {
-        logger.error(t.getMessage(), t);
+    public void error(Object msg, Throwable t) {
+        logger.error(msg, t);
     }
     
 }
