ed8de14f9d3b287734b02501abedb2c5e7b52efb
littlefisher <505516920@qq.com>
2017-01-06 18:42:57 +0800

    暂时保存，还没彻底整理好

---
+++ code/yc-parent/engine-system-parent/engine-system-impl/engine-system-impl/src/main/java/com/yc/engine_system_impl/App.java
@@ -0,0 +1,13 @@
+package com.yc.engine_system_impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ code/yc-parent/engine-system-parent/engine-system-web/engine-system-web/src/main/java/com/yc/engine_system_web/App.java
@@ -0,0 +1,13 @@
+package com.yc.engine_system_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
