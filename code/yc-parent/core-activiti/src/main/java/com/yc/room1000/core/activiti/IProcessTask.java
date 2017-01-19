82e3f398c0dd7d4befa95a402f20d72ef8c95ec5
LittleFisher <505516920@qq.com>
2017-01-19 14:32:56 +0800

    完善activiti

--- code/yc-parent/core-activiti/src/main/java/com/yc/room1000/core/activiti/IProcessTask.java
+++ code/yc-parent/core-activiti/src/main/java/com/yc/room1000/core/activiti/IProcessTask.java
@@ -12,7 +12,7 @@ import java.util.Map;
  * @version 1.0
  * @since v1.0
  */
-public interface IProcessDispatcher {
+public interface IProcessTask {
 
     /**
      * 
@@ -25,4 +25,17 @@ public interface IProcessDispatcher {
      * @param matcher 人工环节的匹配器
      */
     void dispatcherOrder(String linkName, Map<String, Object> variables, ITaskServiceMatcher matcher);
+    
+    /**
+     * 
+     * Description: 人工环节执行逻辑
+     * 
+     * @author jinyanan
+     *
+     * @param linkName 当前人工环节的assignee
+     * @param variables 用来匹配流程实例的参数列表
+     * @param matcher 人工环节的匹配器
+     * @param executer 人工环节的执行器
+     */
+    void executeWork(String linkName, Map<String, Object> variables, ITaskServiceMatcher matcher, ITaskServiceExecuter executer);
 }
---
+++ code/yc-parent/core-activiti/src/main/java/com/yc/room1000/core/activiti/impl/ProcessTaskImpl.java
@@ -0,0 +1,69 @@
+package com.yc.room1000.core.activiti.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.task.Task;
+
+import com.yc.room1000.core.activiti.IProcessTask;
+import com.yc.room1000.core.activiti.ITaskServiceExecuter;
+import com.yc.room1000.core.activiti.ITaskServiceMatcher;
+import com.yc.room1000.core.utils.Room1000Logger;
+
+/**
+ * 
+ * Description: 
+ *  
+ * Created on 2017年1月13日 
+ *
+ * @author jinyanan
+ * @version 1.0
+ * @since v1.0
+ */
+public class ProcessTaskImpl implements IProcessTask {
+    
+    /** logger */
+    private static Room1000Logger logger = Room1000Logger.getLogger(ProcessTaskImpl.class);
+    
+    /** processEngine */
+    private static ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+    @Override
+    public void dispatcherOrder(String linkName, Map<String, Object> variables, ITaskServiceMatcher matcher) {
+        logger.debug("linkName: " + linkName);
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList = processEngine.getTaskService()
+            .createTaskQuery()
+            .taskAssignee(linkName)
+            .list();
+        
+        for (Task task : taskList) {
+            if (matcher.matchTask(variables, taskService.getVariables(task.getId()))) {
+                taskService.complete(task.getId());
+            }
+        }
+    }
+
+    @Override
+    public void executeWork(String linkName, Map<String, Object> variables, ITaskServiceMatcher matcher,
+        ITaskServiceExecuter executer) {
+        logger.debug("linkName: " + linkName);
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> taskList = processEngine.getTaskService().createTaskQuery().taskAssignee(linkName).list();
+
+        for (Task task : taskList) {
+            if (matcher.matchTask(variables, taskService.getVariables(task.getId()))) {
+                Map<String, Object> variablesBefore = taskService.getVariables(task.getId());
+                logger.debug("before execute variables: " + variablesBefore);
+                Map<String, Object> variablesAfter = executer.execute(taskService.getVariables(task.getId()));
+                taskService.setVariables(task.getId(), variablesAfter);
+                // taskService.getVariables(task.getId()).putAll(variablesAfter);
+                logger.debug("after execute variables: " + variablesBefore);
+            }
+        }
+    }
+
+}
