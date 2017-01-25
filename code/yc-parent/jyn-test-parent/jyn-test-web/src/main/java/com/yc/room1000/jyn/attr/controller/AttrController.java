678a4b8f04b7e26d1e99bb1edbf951cf2cb6dacb
LittleFisher <505516920@qq.com>
2017-01-25 14:47:32 +0800

    controller增加注释

--- code/yc-parent/jyn-test-parent/jyn-test-web/src/main/java/com/yc/room1000/jyn/attr/controller/AttrController.java
+++ code/yc-parent/jyn-test-parent/jyn-test-web/src/main/java/com/yc/room1000/jyn/attr/controller/AttrController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
 
 import com.yc.room1000.core.attr.dto.AttrDto;
 import com.yc.room1000.core.attr.service.IAttrService;
@@ -43,6 +44,7 @@ public class AttrController {
      * @return AttrDto
      */
     @RequestMapping(value = "/{attrId}", method = RequestMethod.GET)
+    @ApiOperation(value = "根据attrId获取Attr")
     public AttrDto getAttrById(@PathVariable("attrId") Long attrId) {
         AttrDto attr = attrService.getAttrDtoById(attrId);
         return attr;
