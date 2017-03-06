e4dbdc795dc28187d667d1718c9614978595166e
LittleFisher <505516920@qq.com>
2017-03-06 20:55:42 +0800

    删去工程里的ng2工程

--- code/yc-parent/jyn-test-parent/jyn-test-impl/src/main/java/com/yc/room1000/attr/mapper/AttrDtoMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yc.room1000.attr.mapper;
-
-import com.yc.room1000.attr.model.AttrDto;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface AttrDtoMapper extends Mapper<AttrDto> {
-}
