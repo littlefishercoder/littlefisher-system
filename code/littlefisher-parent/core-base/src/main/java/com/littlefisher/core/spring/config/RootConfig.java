154692a079fe77e4ca17255eb6de281803921503
LittleFisher <505516920@qq.com>
2017-04-18 21:31:23 +0800

    暂时保存

--- code/littlefisher-parent/core-base/src/main/java/com/littlefisher/core/spring/config/RootConfig.java
+++ code/littlefisher-parent/core-base/src/main/java/com/littlefisher/core/spring/config/RootConfig.java
@@ -2,22 +2,27 @@ package com.littlefisher.core.spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
- * 
  * Description: 
- *  
- * Created on 2017年4月18日 
+ * 
+ * Created on 2017年4月18日
  *
  * @author jinyanan
  * @version 1.0
  * @since v1.0
  */
 @Configuration
+@Import(value = {
+        PropertySourceConfig.class, DataSourceConfig.class,
+        SqlSessionFactoryBeanConfig.class, MapperScannerConfigurerConfig.class, 
+        SystemEngineConfig.class, AopConfig.class,
+        TransactionManagerConfig.class })
 @ComponentScan(basePackages = {"com.littlefisher.core.aop", 
         "com.littlefisher.core.interceptor.service",
         "com.littlefisher.**.service.impl",
-        "com.yc.room1000.**.service.impl", })
-public class SpringComponentScanConfig {
+        "com.yc.room1000.**.service.impl" })
+public class RootConfig {
 
 }
---
+++ code/littlefisher-parent/core-base/src/main/java/com/littlefisher/core/spring/config/TransactionManagerConfig.java
@@ -0,0 +1,42 @@
+package com.littlefisher.core.spring.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+/**
+ * 
+ * Description: 
+ *  
+ * Created on 2017年4月18日 
+ *
+ * @author jinyanan
+ * @version 1.0
+ * @since v1.0
+ */
+@Configuration
+public class TransactionManagerConfig {
+    
+    /**
+     * dataSource
+     */
+    @Autowired
+    private DataSource dataSource;
+    
+    /**
+     * 
+     * Description: 
+     * 
+     * @author jinyanan
+     *
+     * @return DataSourceTransactionManager
+     */
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+}
