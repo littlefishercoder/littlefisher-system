789ee364c5ec3a045834b5aecb69c84de5883aab
LittleFisher <505516920@qq.com>
2017-10-05 22:58:33 +0800

    使用Gson来做json解析工作

---
+++ code/core-base/src/main/java/com/littlefisher/core/utils/GsonUtil.java
@@ -0,0 +1,36 @@
+package com.littlefisher.core.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ *
+ * Description: Gson帮助类
+ *
+ * Created on 2017年10月5日
+ *
+ * @author jinyanan
+ * @version 1.0
+ * @since v1.0
+ */
+public class GsonUtil {
+
+    private static Gson gson = new GsonBuilder().serializeNulls()
+            .setDateFormat("yyyy-MM-dd hh:mm:ss").create();
+
+    /**
+     * 禁止实例化
+     */
+    private GsonUtil() {
+    }
+
+    /**
+     * 实体转json
+     *
+     * @param obj 要转的实体
+     * @return String json
+     */
+    public static String obj2Json(Object obj) {
+        return gson.toJson(obj);
+    }
+}
