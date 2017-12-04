8d55e8a8f38308d8cfeecfd531a4fb85551de141
LittleFisher <505516920@qq.com>
2017-12-04 10:20:35 +0800

    单元测试

---
+++ code/blog-parent/blog-web/src/test/java/com/littlefisher/blog/system/SystemParamServiceTest.java
@@ -0,0 +1,43 @@
+package com.littlefisher.blog.system;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.littlefisher.blog.intf.system.ISystemParamService;
+import com.littlefisher.blog.model.system.SystemParamDto;
+import com.littlefisher.blog.request.system.GetSystemParamList4PagerByCondRequest;
+import com.littlefisher.blog.user.UserServiceTest;
+import com.littlefisher.core.utils.LittleFisherLogger;
+
+/**
+ * Description: SystemParamServiceTest.java
+ *
+ * Created on 2017年12月01日
+ *
+ * @author jinyanan
+ * @version 1.0
+ * @since v1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SystemParamServiceTest {
+    private static LittleFisherLogger logger = LittleFisherLogger.getLogger(UserServiceTest.class);
+
+    @Autowired
+    private ISystemParamService systemParamService;
+
+    @Test
+    public void testGetSystemParamList4PagerByCond() {
+        GetSystemParamList4PagerByCondRequest req = new GetSystemParamList4PagerByCondRequest();
+        req.setPageNum(1);
+        req.setPageSize(20);
+        List<SystemParamDto> systemParamList = systemParamService.getSystemParamList4PagerByCond(req);
+        logger.debug("systemParamList: {}", systemParamList);
+    }
+
+}
