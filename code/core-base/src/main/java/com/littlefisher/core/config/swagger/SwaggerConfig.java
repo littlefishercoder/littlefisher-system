c56f00b801ea1bcb3f53195baba4fe773825fa93
LittleFisher <505516920@qq.com>
2018-02-03 20:29:12 +0800

    #26 SwaggerUI的版本升级

--- code/core-base/src/main/java/com/littlefisher/core/config/swagger/SwaggerConfig.java
+++ code/core-base/src/main/java/com/littlefisher/core/config/swagger/SwaggerConfig.java
@@ -1,16 +1,38 @@
 package com.littlefisher.core.config.swagger;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
+import springfox.documentation.schema.WildcardType;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * Description:
@@ -25,17 +47,117 @@ import org.springframework.context.annotation.Configuration;
 @EnableSwagger2
 public class SwaggerConfig {
 
+    @Autowired
+    private TypeResolver typeResolver;
+
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.littlefisher")).paths(PathSelectors.any()).build();
+    public Docket littleFisherApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                // 设置Swagger页面的标题，描述等信息
+                .apiInfo(apiInfo())
+                // 设置哪些注解不被Swagger展示，默认被@ApiIgnore标记的class或者method不被展示
+                .select()
+                // 展示哪些Controller，默认会展示所有的，包括非业务的Controller
+                .apis(RequestHandlerSelectors.basePackage("com.littlefisher.blog.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                // 可以针对原有的RequestMapping前再配置路径
+                .pathMapping("/")
+                .directModelSubstitute(LocalDate.class,
+                        String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .alternateTypeRules(
+                        AlternateTypeRules.newRule(typeResolver.resolve(DeferredResult.class,
+                                typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+                                typeResolver.resolve(WildcardType.class)))
+                .useDefaultResponseMessages(false)
+//                .globalResponseMessage(RequestMethod.GET,
+//                        Lists.newArrayList(new ResponseMessageBuilder()
+//                                .code(500)
+//                                .message("500 message")
+//                                .responseModel(new ModelRef("Error"))
+//                                .build()))
+                // 等有权限校验的时候再加
+//                .securitySchemes(Lists.newArrayList(apiKey()))
+                .securityContexts(Lists.newArrayList(securityContext()))
+//                .enableUrlTemplating(true)
+//                .globalOperationParameters(
+//                        Lists.newArrayList(new ParameterBuilder()
+//                                .name("someGlobalParameter")
+//                                .description("Description of someGlobalParameter")
+//                                .modelRef(new ModelRef("string"))
+//                                .parameterType("query")
+//                                .required(true)
+//                                .build()))
+//                .tags(new Tag("Pet Service", "All apis relating to pets"))
+//                .additionalModels(typeResolver.resolve(AdditionalModel.class))
+                ;
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("mykey", "api_key", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/anyPath.*"))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("mykey", authorizationScopes));
+    }
+
+    @Bean
+    SecurityConfiguration security() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId("test-app-client-id")
+                .clientSecret("test-app-client-secret")
+                .realm("test-app-realm")
+                .appName("test-app")
+                .scopeSeparator(",")
+                .additionalQueryStringParams(null)
+                .useBasicAuthenticationWithAccessCodeGrant(false)
+                .build();
+    }
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+                .deepLinking(true)
+                .displayOperationId(false)
+                .defaultModelsExpandDepth(1)
+                .defaultModelExpandDepth(1)
+                .defaultModelRendering(ModelRendering.EXAMPLE)
+                .displayRequestDuration(false)
+                .docExpansion(DocExpansion.NONE)
+                .filter(false)
+                .maxDisplayedTags(null)
+                .operationsSorter(OperationsSorter.ALPHA)
+                .showExtensions(false)
+                .tagsSorter(TagsSorter.ALPHA)
+                .validatorUrl(null)
+                .build();
+    }
+
+
+
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Littlefisher REST API").description("API Document管理")
-                .termsOfServiceUrl("http://www.littlefisher.site/")
-                .contact(new Contact("LittleFisher", "http://www.littlefisher.site/", "jin68716503@163.com"))
-                .version("1.0").build();
+        return new ApiInfoBuilder()
+                .title("LittleFisher Blog API")
+                .description("LittleFisher Blog后台API调用")
+                .termsOfServiceUrl("https://github.com/littlefishercoder/littlefisher-system")
+                .contact(new Contact("LittleFisher", "http://www.littlefisher.site/", "505516920@qq.com"))
+//                .license("Apache License Version 2.0")
+//                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+                .version("1.0")
+                .build();
     }
 
 }
