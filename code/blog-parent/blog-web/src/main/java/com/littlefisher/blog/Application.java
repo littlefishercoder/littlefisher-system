6486e6fc215ff9e39c8b6dfd65422df017fb0e0d
LittleFisher <505516920@qq.com>
2018-03-15 20:07:25 +0800

    工程升级springboot到2.0.0

--- code/blog-parent/blog-web/src/main/java/com/littlefisher/blog/Application.java
+++ code/blog-parent/blog-web/src/main/java/com/littlefisher/blog/Application.java
@@ -4,12 +4,14 @@ import tk.mybatis.spring.annotation.MapperScan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Description: Application.java
+ * {@link SpringBootApplication#exclude()}中增加{@link SecurityAutoConfiguration}用于关闭security校验
  *
  * Created on 2017年11月17日
  *
@@ -17,7 +19,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @version 1.0
  * @since v1.0
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan("com.littlefisher.**.dao")
 @ComponentScan(basePackages = {
         "com.littlefisher.**",
